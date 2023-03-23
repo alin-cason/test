902caabfddbe885dafce96b48100033fa6696b79
alin.cason <alin.cason@datagroup.ro>
2023-03-23 22:47:54 +0200

    #1 - test upload

---
+++ numara_pagini_pdf/numara_pagini_pdf/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text.factories;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace numara_pagini_pdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string report = "";
+
+            string[] pdfuri = Directory.GetFiles(textBox1.Text, "*.pdf");
+            foreach (string pdf in pdfuri)
+            {
+                PdfReader reader = new PdfReader(pdf);
+
+                int total = reader.NumberOfPages;
+
+                reader.Close();
+
+                report = report + Path.GetFileName(pdf) + "\t" + total.ToString() + "\n";
+            }
+
+            File.WriteAllText(textBox1.Text + "\\__report.txt", report);
+            MessageBox.Show("Done!");
+        }
+    }
+}
